3dde6fa99dc4064f5eecf4af4c8f25cff2b3e743
rks118 <gaoyibobobo+foo@gmail.com>
2023-12-25 20:04:44 +0800

    debuger

--- apps/codex/src/hooks/useInitRepo.ts
+++ apps/codex/src/hooks/useInitRepo.ts
@@ -1,3 +1,4 @@
+import { GreenfieldClient } from '@/config/client';
 import FS from '@isomorphic-git/lightning-fs';
 import git from 'isomorphic-git';
 import { useEffect, useState } from 'react';
@@ -8,7 +9,21 @@ export const useInitRepo = (fs: FS | null) => {
     if (!fs) return;
 
     const init = async () => {
+      // const r = await GreenfieldClient.object.getObject(
+      //   {
+      //     bucketName: 'codex-3-test-repo',
+      //     objectName: 'refs/HEAD',
+      //     endpoint: 'https://gnfd-testnet-sp1.bnbchain.org',
+      //   },
+      //   {
+      //     type: 'ECDSA',
+      //     privateKey: '0x6547492644d0136f76ef65e3bd04a77d079ed38028f747700c6c6063564d7032',
+      //   },
+      // );
+      // console.log('r', r);
+
       console.log('resolveRef');
+      debugger;
       const res = await git.resolveRef({
         fs: fs,
         dir: '',
