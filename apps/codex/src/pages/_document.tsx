e0ab0ca1875d18dc5960612004cf35a2040bb5fb
rks118 <gaoyibobobo+foo@gmail.com>
2024-04-09 15:10:35 +0800

    feat: Airdrop

--- apps/codex/src/pages/_document.tsx
+++ apps/codex/src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Commissioner:wght@300;700&display=swap"
           rel="stylesheet"
         />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </Head>
       <body>
         <Main />
