db58e00cf18b5cfee62e19d8fcce97fcee73bf9a
rks118 <gaoyibobobo+foo@gmail.com>
2024-05-07 19:12:26 +0800

    feat: Check Result

--- apps/codex/src/modules/repo/index.tsx
+++ apps/codex/src/modules/repo/index.tsx
@@ -1,16 +1,28 @@
+import { css, keyframes } from '@emotion/react';
+import Loading from '@/images/loading.svg';
 import { Side } from '@/shared/components/Side';
-import { Center, Flex, Spinner } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, HStack, Spinner, Stack, VStack } from '@chakra-ui/react';
 import { useCheckRepo } from './hooks/useCheckRepo';
 import { useGetBucketInfo } from '@/shared/hooks/gnfd/useGetBucketInfo';
 import { useRouter } from 'next/router';
 import { RepoContent } from './components/RepoContent';
+import { ErrorIcon } from '@/shared/icons/ErrorIcon';
+
+const spin = keyframes`
+  from {
+      transform: rotate(0deg);
+  }
+  to {
+      transform: rotate(360deg);
+  }
+`;
 
 export default function Repo() {
   const router = useRouter();
   const { name } = router.query;
   const { data: bucketInfo } = useGetBucketInfo(name as string);
 
-  const { data: checkRepoRes } = useCheckRepo(bucketInfo?.id);
+  const { data: checkRepoRes, refetch } = useCheckRepo(bucketInfo?.id);
 
   console.log('checkRepoRes', checkRepoRes, checkRepoRes?.result.status);
 
@@ -27,20 +39,35 @@ export default function Repo() {
 
   return (
     <Flex gap="20px" w="1360px" ml="auto" mr="auto">
-      {/* name is {name}
-      latestCommit is {latestCommitOid} */}
-
       {checkRepoRes?.result.status === 11 && (
         <Center minH="200px" w="960px">
-          {/* failure */}
-          <Spinner />
+          <VStack>
+            {/* failure */}
+            <ErrorIcon />
+            <Box color="#F44336">Failed</Box>
+            <Button
+              bg="#048118"
+              fontSize="16px"
+              onClick={() => {
+                refetch();
+              }}
+            >
+              Retry
+            </Button>
+          </VStack>
         </Center>
       )}
 
       {checkRepoRes?.result.status === 1 && (
         <Center minH="200px" w="960px">
-          {/* success */}
-          <Spinner />
+          {/* init */}
+          <Box
+            css={css`
+              animation: ${spin} 2s ease infinite;
+            `}
+          >
+            <img src={Loading.src} title="loading" />
+          </Box>
         </Center>
       )}
 
