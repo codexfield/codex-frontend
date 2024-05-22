245eb03bc8ae786ec4a6559f75252372bdad592b
rks118 <gaoyibobobo+foo@gmail.com>
2024-05-22 22:20:05 +0800

    feat: Add retry

---
+++ apps/codex/src/apis/retry.ts
@@ -0,0 +1,21 @@
+import { AIRDROP_DOMAIN } from '@/env';
+import axios from 'axios';
+
+interface Params {
+  codexBucketId: string;
+  accessToken?: string;
+}
+
+interface IResult {
+  code: number;
+  message: string;
+}
+
+export const retry = async ({ codexBucketId, accessToken }: Params) => {
+  return await axios.get<Promise<IResult>>(`${AIRDROP_DOMAIN}/repo/import`, {
+    params: {
+      codex_bucket_id: codexBucketId,
+      access_token: accessToken,
+    },
+  });
+};
